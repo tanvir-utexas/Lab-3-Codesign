e81a87b95ccb87c47e3bced8a36423a2fe5f63de
tanvirmr-utexas <tanvirmahmud@utexas.edu>
2021-11-06 15:12:45 -0700

    Updates on Quantization

---
+++ quantization.py
@@ -0,0 +1,251 @@
+#Quantisation Functions
+
+from collections import namedtuple
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+from Network import MyConvNet 
+
+
+QTensor = namedtuple('QTensor', ['tensor', 'scale', 'zero_point'])
+
+def calcScaleZeroPoint(min_val, max_val,num_bits=8):
+  # Calc Scale and zero point of next 
+  qmin = 0.
+  qmax = 2.**num_bits - 1.
+
+  scale = (max_val - min_val) / (qmax - qmin)
+
+  initial_zero_point = qmin - min_val / scale
+  
+  zero_point = 0
+  if initial_zero_point < qmin:
+      zero_point = qmin
+  elif initial_zero_point > qmax:
+      zero_point = qmax
+  else:
+      zero_point = initial_zero_point
+
+  zero_point = int(zero_point)
+
+  return scale, zero_point
+
+def quantize_tensor(x, num_bits=8, min_val=None, max_val=None):
+    
+    if not min_val and not max_val: 
+      min_val, max_val = x.min(), x.max()
+
+    qmin = 0.
+    qmax = 2.**num_bits - 1.
+
+    scale, zero_point = calcScaleZeroPoint(min_val, max_val, num_bits)
+    q_x = zero_point + x / scale
+    q_x.clamp_(qmin, qmax).round_()
+    q_x = q_x.round().byte()
+    
+    return QTensor(tensor=q_x, scale=scale, zero_point=zero_point)
+
+def dequantize_tensor(q_x):
+    return q_x.scale * (q_x.tensor.float() - q_x.zero_point)
+
+
+
+#Rework Forward pass of Linear and Conv Layers to support Quantisation
+
+def quantizeLayer(x, layer, stat, scale_x, zp_x):
+  # for both conv and linear layers
+
+  # cache old values
+  W = layer.weight.data
+  B = layer.bias.data
+
+  # quantise weights, activations are already quantised
+  w = quantize_tensor(layer.weight.data) 
+  b = quantize_tensor(layer.bias.data)
+
+  layer.weight.data = w.tensor.float()
+  layer.bias.data = b.tensor.float()
+
+  # This is Quantisation Artihmetic
+  scale_w = w.scale
+  zp_w = w.zero_point
+  scale_b = b.scale
+  zp_b = b.zero_point
+  
+  scale_next, zero_point_next = calcScaleZeroPoint(min_val=stat['min'], max_val=stat['max'])
+
+  # Preparing input by shifting
+  X = x.float() - zp_x
+  layer.weight.data = scale_x * scale_w*(layer.weight.data - zp_w)
+  layer.bias.data = scale_b*(layer.bias.data + zp_b)
+
+  # All int computation
+  x = (layer(X)/ scale_next) + zero_point_next 
+  
+  # Perform relu too
+  x = F.relu(x)
+
+  # Reset weights for next forward pass
+  layer.weight.data = W
+  layer.bias.data = B
+  
+  return x, scale_next, zero_point_next
+
+
+'''Get Max and Min Stats for Quantising Activations of Network.
+This is done by running the network with around 1000 examples 
+and getting the average min and max activation values before and after each layer.'''
+
+
+# Get Min and max of x tensor, and stores it
+def updateStats(x, stats, key):
+  max_val, _ = torch.max(x, dim=1)
+  min_val, _ = torch.min(x, dim=1)
+  
+  
+  if key not in stats:
+    stats[key] = {"max": max_val.sum(), "min": min_val.sum(), "total": 1}
+  else:
+    stats[key]['max'] += max_val.sum().item()
+    stats[key]['min'] += min_val.sum().item()
+    stats[key]['total'] += 1
+  
+  return stats
+
+# Reworked Forward Pass to access activation Stats through updateStats function
+def gatherActivationStats(model, x, stats):
+
+  stats = updateStats(x.clone().view(x.shape[0], -1), stats, 'conv1')
+  x = model.act1(model.bn1((model.conv1(x))))
+  x = model.pool1(x)
+
+  stats = updateStats(x.clone().view(x.shape[0], -1), stats, 'conv2')
+  
+  #x = F.relu(model.conv2(x))
+
+  #x = F.max_pool2d(x, 2, 2)
+
+  x = model.act2(model.bn2((model.conv2(x))))
+  x = model.pool2(x)
+
+#  x = x.view(-1, 4*4*50)
+
+  x = x.view(x.size(0), -1) 
+  stats = updateStats(x, stats, 'lin1')
+
+  x = F.relu(model.lin1(x))
+  
+  stats = updateStats(x, stats, 'lin2')
+
+  x = model.lin2(x)
+
+  return stats
+
+# Entry function to get stats of all functions.
+def gatherStats(model, test_loader):
+    device = 'cuda'
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    stats = {}
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            stats = gatherActivationStats(model, data, stats)
+    
+    final_stats = {}
+    for key, value in stats.items():
+      final_stats[key] = { "max" : value["max"] / value["total"], "min" : value["min"] / value["total"] }
+    return final_stats
+
+
+# Forward Pass for Quantised Inference
+
+def quantForward(model, x, stats):
+  
+  # Quantise before inputting into incoming layers
+  x = quantize_tensor(x, min_val=stats['conv1']['min'], max_val=stats['conv1']['max'])
+
+  x, scale_next, zero_point_next = quantizeLayer(x.tensor, model.conv1, stats['conv2'], x.scale, x.zero_point)
+
+  x = model.act1(model.bn1((model.conv1(x))))
+  x = model.pool1(x)
+
+  print("1 done")
+  
+  x, scale_next, zero_point_next = quantizeLayer(x, model.conv2, stats['lin1'], scale_next, zero_point_next)
+
+  x = model.act2(model.bn2((model.conv2(x))))
+  x = model.pool2(x)
+
+  x = x.view(x.size(0), -1)
+
+  x, scale_next, zero_point_next = quantizeLayer(x, model.lin1, stats['lin2'], scale_next, zero_point_next)
+  
+  # Back to dequant for final layer
+  x = dequantize_tensor(QTensor(tensor=x, scale=scale_next, zero_point=zero_point_next))
+   
+  x = model.lin2(x)
+
+  return x
+
+
+def testQuant(model, test_loader, quant=False, stats=None):
+    device = 'cuda'
+    
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            if quant:
+              pred = quantForward(model, data, stats)
+            else:
+              pred = model(data)
+
+            test_loss += loss_fn(pred, target).item()
+            correct += (pred.argmax(1) == target).type(torch.float).sum().item()
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+
+##starting the process
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 64
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+q_model = torch.load('model.pth').to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(q_model.parameters(), lr=1e-3)
+
+
+#Test on non-quantized model
+testQuant(q_model, test_dataloader, quant=False)
+
+stats = gatherStats(q_model, test_dataloader)
+print(stats)
+
+#test on quantized model
+testQuant(q_model, test_dataloader, quant=True, stats=stats)
--- train.py
+++ train.py
@@ -1,141 +1,90 @@
+from __future__ import print_function
+import argparse
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
-import argparse
-from torch.autograd import Variable
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+from Network import MyConvNet 
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
 
 
-# Hyper Parameters
-input_size = 28
-num_classes = 10
-num_epochs = 1
-batch_size = 32
-learning_rate = 1e-3
-L1_lambda = 0.9
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
 
 
-class MyConvNet(nn.Module):
-    def __init__(self):
-        super(MyConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.act1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(kernel_size=2)
-        self.conv2 = nn.Conv2d(16, 64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.act2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
-        self.lin1 = nn.Linear(7*7*64, 7*7*16)
-        self.lin2 = nn.Linear(7*7*16, 10)
-    
-    def forward(self, x):
-        c1 = self.conv1(x)
-        b1 = self.bn1(c1)
-        a1 = self.act1(b1)
-        p1 = self.pool1(a1)
-        c2 = self.conv2(p1)
-        b2 = self.bn2(c2)
-        a2 = self.act2(b2)
-        p2 = self.pool2(a2)
-        flt = p2.view(p2.size(0), -1)
-        l1 = self.lin1(flt)
-        out = self.lin2(l1)
-        return out
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
 
-model = MyConvNet().to(device)
-
-print(model)
-
-x = torch.randn(batch_size, 1, 28, 28).to(device)
+batch_size = 64
 
 
-print(model(x).shape)
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
 
-train_dataset = datasets.MNIST(root='./Datasets',
-                               train= True,
-                               transform= transforms.ToTensor(),
-                               download = True) 
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-test_dataset = datasets.MNIST(root='./Datasets',
-                              train = False,
-                              transform = transforms.ToTensor(),
-                              download= True)
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-test_loader = DataLoader(dataset=test_dataset,
-                         batch_size = batch_size,
-                         shuffle = False)
-
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
-
-
-def check_accuracy(loader, model):
-  num_correct = 0
-  num_samples = 0
-
-  model.eval()
-
-  with torch.no_grad():
-    for batch, (x, y) in enumerate(loader):
-      x = Variable(x)
-      y = Variable(y)
-
-      x = x.to(device=device)
-      y = y.to(device=device)
-
-      scores = model(x)
-
-      _, predictions = scores.max(1)
-      
-      num_correct += (predictions == y).sum()
-      num_samples += predictions.size(0)
-      
-
-    print(f"Got {num_correct}/ {num_samples} with accuray {float(num_correct)/float(num_samples)*100:0.2f}")
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-for epoch in range(num_epochs):
-  for batch_idx, (data, targets) in enumerate(train_loader):
-    data = Variable(data)
-    targets = Variable(targets)
+if __name__ == '__main__':
+    # Create data loaders.
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-    data = data.to(device=device)
-    targets = targets.to(device=device)
-    
-    scores = model(data)
-    loss = criterion(scores, targets)
+    model = MyConvNet().to(device)
+    print(model)
 
-    #Adding L1_norm here
-    for name, param in model.named_parameters():
-      if 'weight' in name: 
-        L1_1 = Variable(param, requires_grad= True)
-        L1_2 = torch.norm(L1_1, 1)
-        L1_3 = L1_lambda * L1_2
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-        loss = loss + L1_3;       
-        #print(L1_2)
 
-    optimizer.zero_grad()
-    loss.backward()
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
 
-    optimizer.step()
-
-    if (batch_idx+1) % 100 == 0:
-      print('Epoch: [{}/{}], Step: [{}/{}], Loss: {:0.4f}'. format(epoch + 1, num_epochs, batch_idx + 1, len(train_dataset) // batch_size, loss.data.item()))
-    
-  print(f"Epoch:{epoch+1}, Training accuracy:")
-  check_accuracy(train_loader, model)
-
-  print(f"Epoch:{epoch+1}, Testing accuracy:")
-  check_accuracy(test_loader, model)
-
-  torch.save(model, '/home/tmahmud/My_codes/Lab3/MyConvNet.pth')
+    torch.save(model, "model.pth")
